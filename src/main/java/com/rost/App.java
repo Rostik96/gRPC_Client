56a8b9bd13c6e68616cbc719412112182be0c43e
Rost <coderost@yandex.ru>
2023-01-28 22:05:03 +0300

    Write grpc logic into main-method.

--- src/main/java/com/rost/App.java
+++ src/main/java/com/rost/App.java
@@ -1,11 +1,27 @@
 package com.rost;
 
+import com.rost.grpc.GreetingServiceGrpc;
+import com.rost.grpc.GreetingServiceOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext()
+                .build();
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingServiceOuterClass.HelloRequest req = GreetingServiceOuterClass.HelloRequest.newBuilder()
+                .setName("Neil")
+                .build();
+
+        GreetingServiceOuterClass.HelloResponse resp = stub.greeting(req);
+        System.out.println(resp);
+        channel.shutdownNow();
     }
 }
